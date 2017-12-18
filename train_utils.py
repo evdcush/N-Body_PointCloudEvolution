cc326e92bb308445e4f0e514db746c16616db713
Evan <evdcush@gmail.com>
2017-12-17 22:38:47 -0800

    need sleep

--- train_utils.py
+++ train_utils.py
@@ -10,6 +10,7 @@ import data_utils
 from chainer.datasets import TupleDataset
 import chainer.training as training
 from chainer.dataset import iterator
+from chainer.iterators import SerialIterator
 #from chainer.training import Updater
 
 #tupset = TupleDataset(x,y) # tupset[i] == x[i],y[i]
@@ -23,26 +24,16 @@ class nBodyUpdater(training.Updater):
 
 
 
-class NbodyIterator(iterator.Iterator):
+class nBodyIterator(iterator.Iterator):
     '''
     NOTE: only modification from chainer.iterators.SerialIterator is 
      the data shifting procedure in __next__
     '''
-
     """Dataset iterator that serially reads the examples.
 
     This is a simple implementation of :class:`~chainer.dataset.Iterator`
     that just visits each example in either the order of indexes or a shuffled
     order.
-
-    To avoid unintentional performance degradation, the ``shuffle`` option is
-    set to ``True`` by default. For validation, it is better to set it to
-    ``False`` when the underlying dataset supports fast slicing. If the
-    order of examples has an important meaning and the updater depends on the
-    original order, this option should be set to ``False``.
-
-    This iterator saves ``-1`` instead of ``None`` in snapshots since some
-    serializers do not support ``None``.
 
     Args:
         dataset: Dataset to iterate.
@@ -54,101 +45,40 @@ class NbodyIterator(iterator.Iterator):
             order of indexes.
 
     """
-
-    def __init__(self, dataset, batch_size, repeat=True, shuffle=True):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self._repeat = repeat
-        self._shuffle = shuffle
-
-        self.reset()
+    def peturb_data(self, batch):
+        # batch is tuple (x,y) where x.shape == y.shape == (mb_size, N, D)
+        batch_size = batch[0].shape[0]
+        rands = np.random.rand(6)
+        shift = np.random.rand(batch_size,3)
+        out = []
+        for arr in batch:
+            tmp = arr
+            if rands[0] < .5:
+                tmp = tmp[:,:,[1,0,2,4,3,5]]
+            if rands[1] < .5:
+                tmp = tmp[:,:, [0,2,1,3,5,4]]
+            if rands[2] < .5:
+                tmp = tmp[:,:, [2,1,0,5,4,3]]
+            if rands[3] < .5:
+                tmp[:,:,0] = 1 - tmp[:,:,0]
+                tmp[:,:,3] = -tmp[:,:,3]
+            if rands[4] < .5:
+                tmp[:,:,1] = 1 - tmp[:,:,1]
+                tmp[:,:,4] = -tmp[:,:,4]
+            if rands[5] < .5:
+                tmp[:,:,2] = 1 - tmp[:,:,2]
+                tmp[:,:,5] = -tmp[:,:,5]            
+            tmploc = tmp[:,:,:3]
+            tmploc += shift[:,None,:]
+            gt1 = tmploc > 1
+            tmploc[gt1] = tmploc[gt1] - 1
+            tmp[:,:,:3] = tmploc
+            out.append(tmp)
+        return tuple(out)
 
     def __next__(self):
-        if not self._repeat and self.epoch > 0:
-            raise StopIteration
-
-        self._previous_epoch_detail = self.epoch_detail
-
-        i = self.current_position
-        i_end = i + self.batch_size
-        N = len(self.dataset)
-
-        if self._order is None:
-            batch = self.dataset[i:i_end]
-        else:
-            batch = [self.dataset[index] for index in self._order[i:i_end]]
-
-        if i_end >= N:
-            if self._repeat:
-                rest = i_end - N
-                if self._order is not None:
-                    numpy.random.shuffle(self._order)
-                if rest > 0:
-                    if self._order is None:
-                        batch.extend(self.dataset[:rest])
-                    else:
-                        batch.extend([self.dataset[index]
-                                      for index in self._order[:rest]])
-                self.current_position = rest
-            else:
-                self.current_position = 0
-
-            self.epoch += 1
-            self.is_new_epoch = True
-        else:
-            self.is_new_epoch = False
-            self.current_position = i_end
-
-        return batch
-
-    next = __next__
-
-    @property
-    def epoch_detail(self):
-        return self.epoch + self.current_position / len(self.dataset)
-
-    @property
-    def previous_epoch_detail(self):
-        if self._previous_epoch_detail < 0:
-            return None
-        return self._previous_epoch_detail
-
-    def serialize(self, serializer):
-        self.current_position = serializer('current_position',
-                                           self.current_position)
-        self.epoch = serializer('epoch', self.epoch)
-        self.is_new_epoch = serializer('is_new_epoch', self.is_new_epoch)
-        if self._order is not None:
-            try:
-                serializer('order', self._order)
-            except KeyError:
-                serializer('_order', self._order)
-        try:
-            self._previous_epoch_detail = serializer(
-                'previous_epoch_detail', self._previous_epoch_detail)
-        except KeyError:
-            # guess previous_epoch_detail for older version
-            self._previous_epoch_detail = self.epoch + \
-                (self.current_position - self.batch_size) / len(self.dataset)
-            if self.epoch_detail > 0:
-                self._previous_epoch_detail = max(
-                    self._previous_epoch_detail, 0.)
-            else:
-                self._previous_epoch_detail = -1.
-
-    def reset(self):
-        if self._shuffle:
-            self._order = numpy.random.permutation(len(self.dataset))
-        else:
-            self._order = None
-
-        self.current_position = 0
-        self.epoch = 0
-        self.is_new_epoch = False
-
-        # use -1 instead of None internally.
-        self._previous_epoch_detail = -1.
-
+        batch = super(nBodyIterator, self).__next__()
+        return self.peturb_data(batch)
 
 #=============================================================================
 # network layers
