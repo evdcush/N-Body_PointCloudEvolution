b56d30bb5d4c2b588152482a531ae3cb1c5048ff
Evan <evdcush@gmail.com>
2018-01-22 17:23:30 -0800

    added simpler .py training script

---
+++ train.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import time
+import argparse
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import cuda
+import numpy as np
+import cupy
+
+import models
+import nn
+import graph_ops
+import data_utils
+
+# static vars
+RNG_SEEDS     = [98765, 12345, 319, 77743196]
+BOUND         = 0.095
+LEARNING_RATE = 0.01
+GRAPH_CHANNELS = [6, 8, 16, 32, 16, 8, 3, 8, 16, 32, 16, 8, 3] # for graph model
+SET_CHANNELS   = [6, 32, 128, 256, 128, 32, 256, 16, 3]
+CHANNELS = {0:SET_CHANNELS, 1:GRAPH_CHANNELS, 2:None}
+NBODY_MODELS = {0:models.SetModel, 1:models.GraphModel, 2:models.VelocityScaled}
+MTAGS = {0:'S', 1:'G', 2:'V'}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batchsize', '-b', default=8,          type=int, help='batch size')
+parser.add_argument('--num_iters', '-i', default=3000,       type=int, help='number of training iterations')
+parser.add_argument('--model_dir', '-s', default='./Model/',           help='directory where you can find a file storing model information')
+parser.add_argument('--model_name','-n', default='',                   help='model name')
+parser.add_argument('--model_type','-m', default=0,          type=int, help='model type, 0:set, 1:graph, 2:vel')
+parser.add_argument('--use_theta', '-t', default=0,          type=int, help='if 1, use theta timestep coeff')
+parser.add_argument('--gpu_use',   '-g', default=1,          type=int, help='use gpu if 1, else cpu')
+parser.add_argument('--particles', '-p', default=16,         type=int, help='number of particles, dataset')
+parser.add_argument('--redshifts', '-r', nargs='+',          type=int, help='redshift tuple')
+args = parser.parse_args()
+
+#=============================================================================
+# Training and model params, from arg parser
+#=============================================================================
+use_gpu = True if args.gpu_use == 1 else False
+xp = cupy if use_gpu == 1 else np
+mb_size   = args.batchsize # 8
+num_iters = args.num_iters # 3000
+num_particles = args.particles
+zX, zY = args.redshifts
+mtype  = NBODY_MODELS[args.model_type]
+channels = CHANNELS[args.model_type]
+mname  = args.model_name
+theta = None
+save_label = data_utils.get_save_label(mname, MTAGS[mtype], args.use_theta, num_particles, zX, zY)
+if args.use_theta == 1:
+    thetas = np.load('./thetas_timesteps.npy').item()
+    theta_val = thetas[(num_particles, zX, zY)]['W']
+    theta = theta_val
+
+model_dir = args.model_dir
+loss_path = model_dir + 'Loss/'
+if not os.path.exists(loss_path): os.makedirs(loss_path)
+
+
+
+def seed_rng(s=12345):
+    np.random.seed(s)
+    xp.random.seed(s)
+
+seed_rng()
+
+#=============================================================================
+# Load data
+#=============================================================================
+X, Y = data_utils.load_data(num_particles, zX, zY, normalize_data=True)
+if use_gpu:
+    X = cuda.to_gpu(X)
+    Y = cuda.to_gpu(Y)
+X_tup, Y_tup = data_utils.split_data_validation(X,Y, num_val_samples=200)
+X_train, X_val = X_tup
+Y_train, Y_val = Y_tup
+
+#=============================================================================
+# Loss history
+#=============================================================================
+train_loss_history = np.zeros((len(RNG_SEEDS), num_iters))
+num_val_batches = X_val.shape[0] // mb_size
+validation_loss_history = np.zeros((len(RNG_SEEDS), num_val_batches))
+
+#=============================================================================
+# Training
+#=============================================================================
+for rng_idx, rseed in enumerate(RNG_SEEDS):
+    model_save_label = save_label + '{}_'.format(rseed)
+    seed_rng(rseed)
+    # setup model
+    model = mtype(channels)
+    if use_gpu:
+        model.to_gpu()
+    # setup optimizer
+    optimizer = optimizers.Adam(alpha=LEARNING_RATE)
+    optimizer.setup(model)
+
+    # setup loss trackers
+    lh_train = np.zeros((train_loss_history.shape[-1]))
+    lh_val   = np.zeros((validation_loss_history.shape[-1]))
+    
+    
+    # train loop
+    for cur_iter in range(num_iters):
+        model.zerograds() # must always zero grads before another forward pass!
+
+        # create mini-batches for training
+        _x_in, _x_true = data_utils.next_minibatch([X,Y], mb_size)
+        x_in, x_true = chainer.Variable(_x_in), chainer.Variable(_x_true)
+
+        # get prediction and loss
+        x_hat = model(x_in, add=True) # prediction
+        loss = nn.mean_squared_error(x_hat, x_true, boundary=BOUND) # bound = 0.095                
+        
+        # backprop and update
+        loss.backward() # this calculates all the gradients (backprop)
+        optimizer.update() # this updates the weights
+
+        lh_train[cur_iter] = cuda.to_cpu(loss.data)
+    train_loss_history[rng_idx] = lh_train
+    np.save(loss_path + save_label + 'train_loss', train_loss_history)
+    print('{}: converged at {}'.format(model_save_label, np.median(lh_train[-200:])))
+    # save model, optimizer
+    data_utils.save_model([model, optimizer], model_dir + model_save_label)
+
+    # validation
+    with chainer.using_config('train', False):
+        for val_iter in range(num_val_batches):
+            j,k = val_iter * mb_size, (val_iter+1) * mb_size
+            _val_in   = xp.copy(X_val[j:k])
+            _val_true = xp.copy(Y_val[j:k])
+            val_in, val_true = chainer.Variable(_val_in), chainer.Variable(_val_true)
+
+            val_hat  = model(val_in, add=True)
+            val_loss = nn.mean_squared_error(val_hat, val_true, boundary=BOUND)
+            lh_val[val_iter] = cuda.to_cpu(val_loss.data)
+        validation_loss_history[rng_idx] = lh_val
+        np.save(loss_path + save_label + 'val_loss', validation_loss_history)
+        print('{}: validation avg {}'.format(model_save_label, np.mean(lh_val)))
+    model = optimizer = lh_train = lh_val = None
+
+print('{}: averaged convergence at {}'.format(save_label, np.median(np.mean(train_loss_history, axis=0))[-200:]))
+print('{}'.format(save_label))
