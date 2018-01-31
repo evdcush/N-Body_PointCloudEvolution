ac0b65f86f102c3bc6b322b7cd87cc9456cb2d9c
Evan <evdcush@gmail.com>
2018-01-31 11:29:27 -0800

    rs model

--- models.py
+++ models.py
@@ -94,101 +94,61 @@ class VelocityScaled(chainer.Chain):
 #=============================================================================
 
 class RSModel(chainer.Chain):
-    """ Base model class, defines basic functionality all models
-    """
-    rs_num = 10 # 11 redshifts, so 10 possible predictions
-    def __init__(self, channels, *args, theta=None):
+    BOUND = (0.095, 1-0.095)
+    def __init__(self, channels, *args, layer=SetModel, theta=None, n_P=16):
         self.channels = channels
         last_channels = channels[:-1] + [3,]
+        self.tags = ['6040', '4020', '2015', '1512', '1210', '1008', '0806', '0604', '0402', '0200']
+        theta_rs = [6.0, 4.0, 2.0, 1.5, 1.2, 1.0, 0.8, 0.6, 0.4, 0.2, 0.0]
+        theta_val = None
+        super(RSModel, self).__init__()
+        for i in range(len(self.tags)):
+            cur_tag = self.tags[i]
+            if theta is not None:
+                theta_val = theta[(n_P, theta_rs[i], theta_rs[i+1])]['W']
+            self.add_link('RS_' + cur_tag, layer(channels, theta=theta_val))
         
-        super(RSModel, self).__init__(
-            RS_6040 = models.SetModel(channels, theta=theta),
-            RS_4020 = models.SetModel(channels, theta=theta),
-            RS_2015 = models.SetModel(channels, theta=theta),
-            RS_1512 = models.SetModel(channels, theta=theta),
-            RS_1210 = models.SetModel(channels, theta=theta),
-            RS_1008 = models.SetModel(channels, theta=theta),
-            RS_0806 = models.SetModel(channels, theta=theta),
-            RS_0604 = models.SetModel(channels, theta=theta),
-            RS_0402 = models.SetModel(channels, theta=theta),
-            RS_0200 = models.SetModel(last_channels, theta=theta),
-            )
-    def fwd_pred(self, x):
-        # each layer receives previoous layer prediction
-        rs_40_hat = self.RS_6040(x)
-        #print('input: {}, output: {}'.format(x.shape, rs_40_hat.shape))
-        rs_20_hat = self.RS_4020(rs_40_hat)
-        rs_15_hat = self.RS_2015(rs_20_hat)
-        rs_12_hat = self.RS_1512(rs_15_hat)
-        rs_10_hat = self.RS_1210(rs_12_hat)
-        rs_08_hat = self.RS_1008(rs_10_hat)
-        rs_06_hat = self.RS_0806(rs_08_hat)
-        rs_04_hat = self.RS_0604(rs_06_hat)
-        rs_02_hat = self.RS_0402(rs_04_hat)
-        rs_00_hat = self.RS_0200(rs_02_hat)
-        return rs_00_hat
+    def fwd_pred(self, x, rs_tup=(0,10)):
+        """
+        # each layer receives previoous layer prediction        
+        rs_tup is idx of redshift
+        if rs_tup == (7, 10), then you are making prediction from redshift 0.6 to 0.0
+        """
+        # x.shape == (1, n_P, 6)
+        rs_start, rs_target = rs_tup
+        redshift_distance = rs_target - rs_start
+        assert redshift_distance > 0 and rs_target <= 10
+        predictions = self.xp.zeros(((redshift_distance,) + x.shape)).astype(self.xp.float32)
+        cur_layer = getattr(self, 'RS_' + self.tags[rs_start])
+        hat = cur_layer(x)
+        predictions[0] = hat.data
+        if redshift_distance == 1:
+            return hat, predictions
+        else:
+            for i in range(rs_start+1, rs_target):
+                cur_layer = getattr(self, 'RS_' + self.tags[i])
+                hat = cur_layer(hat)
+                predictions[i] = hat.data
+            return hat, predictions
     
     def fwd_input(self, x):
         """ each layer receives external input
-        x.shape = (11, bs, n_P, 6)
         """
-        # each layer receives data input
-        rs_40_hat = self.RS_6040(x[0])
-        rs_20_hat = self.RS_4020(x[1])
-        rs_15_hat = self.RS_2015(x[2])
-        rs_12_hat = self.RS_1512(x[3])
-        rs_10_hat = self.RS_1210(x[4])
-        rs_08_hat = self.RS_1008(x[5])
-        rs_06_hat = self.RS_0806(x[6])
-        rs_04_hat = self.RS_0604(x[7])
-        rs_02_hat = self.RS_0402(x[8])
-        rs_00_hat = self.RS_0200(x[9])
-        return rs_00_hat
-
-    def fwd_input_pred_diff(self, x):
+        hat = self.RS_6040(x[0])
+        error = nn.get_bounded_MSE(hat, x[1], boundary=self.BOUND)
+        for i in range(1, len(self.tags)):
+            cur_layer = getattr(self, 'RS_' + self.tags[i])
+            hat = cur_layer(x[i])
+            error += nn.get_bounded_MSE(hat, x[i+1], boundary=self.BOUND)
+        return hat, error
+    
+    def fwd_pred_loss(self, x):
         """ each layer receives external input
-        x.shape = (11, bs, n_P, 6)
         """
-        rs_40_hat = self.RS_6040(x[0])
-        diff_40 = F.abs(rs_40_hat - x[1])
-
-        rs_20_hat = self.RS_4020(diff_40)
-        diff_20 = F.abs(rs_20_hat - x[2])
-
-        rs_15_hat = self.RS_2015(diff_20)
-        diff_15 = F.abs(rs_15_hat - x[3])
-
-        rs_12_hat = self.RS_1512(diff_15)
-        diff_12 = F.abs(rs_12_hat - x[4])
-
-        rs_10_hat = self.RS_1210(diff_12)
-        diff_10 = F.abs(rs_10_hat - x[5])
-
-        rs_08_hat = self.RS_1008(diff_10)
-        diff_08 = F.abs(rs_08_hat - x[6])
-
-        rs_06_hat = self.RS_0806(diff_08)
-        diff_06 = F.abs(rs_06_hat - x[7])
-
-        rs_04_hat = self.RS_0604(diff_06)
-        diff_04 = F.abs(rs_04_hat - x[8])
-
-        rs_02_hat = self.RS_0402(diff_02)
-        diff_02 = F.abs(rs_02_hat - x[9])
-
-        rs_00_hat = self.RS_0200(diff_02)
-        return rs_00_hat
-    
-    
-    
-    def __call__(self, x, *args, pred_fwd=0):
-        """
-        x.shape == (1, n_P, 6) # just the 6.0 sample
-        returns (1, n_P, 3) # the prediction on 0.0
-        """
-        if pred_fwd == 0:
-            return self.fwd_input(x)
-        elif pred_fwd == 1:
-            return self.fwd_pred(x)
-        elif pred_fwd == 2:
-            return self.fwd_input_pred_diff(x)
+        hat = self.RS_6040(x[0])
+        error = nn.get_bounded_MSE(hat, x[1], boundary=self.BOUND)
+        for i in range(1, len(self.tags)):
+            cur_layer = getattr(self, 'RS_' + self.tags[i])
+            hat = cur_layer(hat)
+            error += nn.get_bounded_MSE(hat, x[i+1], boundary=self.BOUND)
+        return hat, error
