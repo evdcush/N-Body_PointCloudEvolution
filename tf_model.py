0bbdd7846c711ae7a9cd1eea73fc7900398094bc
Evan <evdcush@gmail.com>
2018-02-13 21:54:17 -0800

    make .py script for tf stuff

---
+++ tf_model.py
@@ -0,0 +1,7 @@
+import os, code, sys, time
+import numpy as np
+import tensorflow as tf
+import chainer.functions as F
+from sklearn.neighbors import kneighbors_graph
+import matplotlib.pyplot as plt
+import utils
