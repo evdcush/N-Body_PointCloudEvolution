0b76b5f6ab424e00dc71212496c657988a2e265f
Evan <evdcush@gmail.com>
2018-02-14 17:13:59 -0800

    recovered original utils.py script for chainer model

--- utils.py
+++ utils.py
@@ -1,14 +1,16 @@
 import os, glob, struct, code, sys, shutil, time
 
 import numpy as np
+import cupy
 import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 import chainer
+from chainer import cuda
 import chainer.serializers as serializers
 
-import tensorflow as tf
+import models, nn
 
 #=============================================================================
 # Globals
@@ -20,66 +22,73 @@ REDSHIFTS = [6.0, 4.0, 2.0, 1.5, 1.2, 1.0, 0.8, 0.6, 0.4, 0.2, 0.0]
 RS_TAGS = {6.0:'60', 4.0:'40', 2.0:'20', 1.5:'15', 1.2:'12', 1.0:'10', 0.8:'08', 0.6:'06', 0.4:'04', 0.2:'02', 0.0:'00'}
 
 # rng seeds
+#RNG_SEEDS     = [98765, 12345, 319, 77743196] # original training seeds for network params
 PARAMS_SEED  = 77743196 # best consistent performance for graph models, set models do better with 98765
 DATASET_SEED = 12345 # for train/validation data splits
 
 # models
 GRAPH_CHANNELS = [6, 8, 16, 32, 16, 8, 3, 8, 16, 32, 16, 8, 3]
 SET_CHANNELS   = [6, 32, 128, 256, 128, 32, 256, 16, 3]
+NBODY_MODELS = {0:{'mclass': models.SetModel,       'channels':   SET_CHANNELS, 'tag': 'S', 'loss':nn.bounded_mean_squared_error},
+                1:{'mclass': models.GraphModel,     'channels': GRAPH_CHANNELS, 'tag': 'G', 'loss':nn.get_min_readout_MSE},
+                2:{'mclass': models.VelocityScaled, 'channels': GRAPH_CHANNELS, 'tag': 'V', 'loss':nn.bounded_mean_squared_error},}
 LEARNING_RATE = 0.01
 
-WEIGHT_TAG = 'W_{}'
-BIAS_TAG   = 'B_{}'
-VAR_SCOPE  = 'params'
 
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-# TF-related utils
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-
-#=============================================================================
-# var inits
-#=============================================================================
-def init_weight(k_in, k_out, name, scale=1.0, seed=None):
-    """ initialize weight Variable
-    weight values drawn from He normal distribution
-    Args:
-        k_in, k_out (int): weight sizes
-        name (str): variable name
-    """
-    std = scale * np.sqrt(2. / k_in)
-    henorm = tf.random_normal((k_in, k_out), stddev=std, seed=seed)
-    with tf.variable_scope(VAR_SCOPE):
-        tf.get_variable(name, dtype=tf.float32, initializer=henorm)
-
-def init_bias(k_in, k_out, name):
-    """ biases initialized to be near zero
-    """
-    bval = tf.ones((k_out,), dtype=tf.float32) * 1e-6
-    with tf.variable_scope(VAR_SCOPE):
-        tf.get_variable(name, dtype=tf.float32, initializer=bval)
-
-def init_params(kdims, seed=98765):
-    for idx, ktup in enumerate(kdims):
-        init_weight(*ktup, WEIGHT_TAG.format(idx), seed=seed)
-        init_bias(  *ktup,   BIAS_TAG.format(idx))
-
-def get_layer_vars(layer_idx):
-    """ gets variables for layer
-    """
-    with tf.variable_scope(VAR_SCOPE, reuse=True):
-        W = tf.get_variable(WEIGHT_TAG.format(layer_idx))
-        B = tf.get_variable(  BIAS_TAG.format(layer_idx))
-    return W, B
+'''
+refactor notes:
+ - util functions duplicated:
+    - npy binaries vs struct binaries
+        - data is no longer loaded from struct binaries,
+          but functions still used
+          - IGNORE: others still load from struct binaries
+    - multi-step rs model vs single-step
+        - this is really bad in train.py, too many conditionals
+          - but unless you make a chainer.Trainer, Updater, and
+            all that, it's simpler to keep train script consistent
+            between models
+ - Too many calls to cuda.get_array_module
+    - could be two birds one stone here, just make multi-rs funs
+      interface the single-step funs.
+ - should split npy data into redshifts. If you aren't training
+   the multi-rs model, you still end up loading the entire dataset
+   into memory (requires 10gb memory), and then selecting
+   just two redshifts.
+ - similarly, training and validation should be saved, rather than
+   relying on seeds
+ - pyplot cruft. Notebooks no longer used.
+ - 'from params import *' is disgusting, stop
+ - sandbox notebook is messy, undocumented spaghetti. commonly
+   referenced code blocks:
+     - how to display plots in notebook (iPython display module)
+     - validation loop (this should be it's own notebook or .py)
+     - matplotlib mplot3d use
+     - compared error plots
+ - the ugliest code right now is having to split train code
+   for multi-step rs model vs normal model
 
 
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-# END TF-related utils
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><
-
+Chainer idiosyncrasies:
+ - like TF, really doesn't like classes that don't inherit
+   from some chainer base class. Models would not converge
+   or learn with custom trainer and dataset management classes.
+   Seems to disrupt the gradient chaining. Stick with explicit
+   train loop and functions for now.
+ - where data is passed as array module (Variable.data), whether
+   it be passed by the caller as an argument, or done within callee
+   scope is not arbitrary. In models.GraphModel, the
+   neighbor the input data MUST be converted back to numpy array
+   before being sent to nn.KNN. Calling cuda.to_cpu(x.data) in
+   nn.KNN broke the model, perhaps because models.GraphModel is
+   a chainer.Chain.
+      - note: maybe remove the *NN classes in nn.py altogether?
+              They were only separated for convenience at the
+              beginning of the project, so that I could evaluate
+              the graphs and search methods apart from having to
+              create a model. It actually might improve performance,
+              considering nn.KNN is not a chainer child class.
+              There's no need for the class, just take the functions out
+'''
 #=============================================================================
 # Loading utils
 #=============================================================================
@@ -150,19 +159,20 @@ def normalize(X_in, scale_range=(0,1)):
         X_in (ndarray): data to be normalized, of shape (N, D, 6)
         scale_range   : range to which coordinate data is rescaled
     """
-    x_r = np.reshape(X_in, [-1,6])
-    coo, vel = np.split(x_r, [3], axis=-1)
+    xp = chainer.cuda.get_array_module(X_in)
+    x_r = xp.reshape(X_in, [-1,6])
+    coo, vel = xp.split(x_r, [3], axis=-1)
 
-    coo_min = np.min(coo, axis=0)
-    coo_max = np.max(coo, axis=0)
+    coo_min = xp.min(coo, axis=0)
+    coo_max = xp.max(coo, axis=0)
     a,b = scale_range
     x_r[:,:3] = (b-a) * (x_r[:,:3] - coo_min) / (coo_max - coo_min) + a
 
-    vel_mean = np.mean(vel, axis=0)
-    vel_std  = np.std( vel, axis=0)
+    vel_mean = xp.mean(vel, axis=0)
+    vel_std  = xp.std( vel, axis=0)
     x_r[:,3:] = (x_r[:,3:] - vel_mean) / vel_std
 
-    X_out = np.reshape(x_r,X_in.shape).astype(np.float32) # just convert to float32 here
+    X_out = xp.reshape(x_r,X_in.shape).astype(xp.float32) # just convert to float32 here
     return X_out
 
 def normalize_fullrs(X, scale_range=(0,1)):
@@ -176,38 +186,147 @@ def normalize_fullrs(X, scale_range=(0,1)):
     """
     for rs_idx in range(X.shape[0]):
         X[rs_idx] = normalize(X[rs_idx])
+    '''
+    xp = chainer.cuda.get_array_module(X)
+    for rs_idx in range(X.shape[0]):
+        X_z = X[rs_idx]
+        x_r = xp.reshape(X_z, [-1, 6])
+        coo, vel = xp.split(x_r, [3], axis=-1)
+
+        # rescale coordinates
+        coo_min = xp.min(coo, axis=0)
+        coo_max = xp.max(coo, axis=0)
+        a,b = scale_range
+        x_r[:,:3] = (b-a) * (x_r[:,:3] - coo_min) / (coo_max - coo_min) + a
+
+        # normalize velocities
+        vel_mean = xp.mean(vel, axis=0)
+        vel_std  = xp.std( vel, axis=0)
+        x_r[:,3:] = (x_r[:,3:] - vel_mean) / vel_std
+
+        # reshape current redshift x, and assign
+        X_z = xp.reshape(x_r, X_z.shape).astype(xp.float32) # safe redundant cast to float32
+        X[rs_idx] = X_z
+    '''
     return X
 
-
-def split_data_validation(X, Y, num_val_samples=200, seed=None):
+def split_data_validation(X, Y, num_val_samples=200):
     """ split dataset into training and validation sets
 
     Args:
         X, Y (ndarray): data arrays of shape (num_samples, num_particles, 6)
         num_val_samples (int): size of validation set
-    Returns: tuple([X_train, X_val], [Y_train, Y_val])
     """
     num_samples = X.shape[0]
-    if seed is not None: np.random.seed(seed)
     idx_list    = np.random.permutation(num_samples)
-    X = np.split(X[idx_list], [-num_val_samples])
-    Y = np.split(Y[idx_list], [-num_val_samples])
-    return X, Y
+    X, Y = X[idx_list], Y[idx_list]
+    X_input, X_val = X[:-num_val_samples], X[-num_val_samples:]#np.split(X, [-num_val_samples])
+    X_truth, Y_val = Y[:-num_val_samples], Y[-num_val_samples:]#np.split(Y, [-num_val_samples])
+    return [(X_input, X_val), (X_truth, Y_val)]
 
-
-def multi_split_data_validation(X, num_val_samples=200, seed=None):
+def multi_split_data_validation(X, num_val_samples=200):
     """ split dataset into training and validation sets
 
     Args:
         X (ndarray): data arrays of shape (num_rs, num_samples, num_particles, 6)
         num_val_samples (int): size of validation set
     """
-    if seed is not None: np.random.seed(seed)
     idx_list = np.random.permutation(X.shape[1])
     X = X[:,idx_list]
     X_train = X[:, :-num_val_samples]
     X_val   = X[:, -num_val_samples:]
     return X_train, X_val
+
+def rotate_x(sin_t, cos_t, x, y, z):
+    out_x = x
+    out_y = cos_t * y - sin_t * z
+    out_z = sin_t * y + cos_t * z
+    return out_x, out_y, out_z
+
+def rotate_y(sin_t, cos_t, x, y, z):
+    out_x = cos_t * x + sin_t * z
+    out_y = y
+    out_z = -sin_t * x + cos_t * z
+    return out_x, out_y, out_z
+
+def rotate_z(sin_t, cos_t, x, y, z):
+    out_x = cos_t * x - sin_t * y
+    out_y = sin_t * x + cos_t * y
+    out_z = z
+    return out_x, out_y, out_z
+
+def rotate_3D(thetas, input_data):
+    """ Rotate data by random theta
+    thetas and input_data will be split by D axis, each retaining it's split
+    axis dim (1,)
+    sin, cos are performed once at the caller level, and the callee rotate
+    functions only perform the rotational algebra
+
+    Args:
+        thetas     (ndarray): (mb_size, 3) random numbers for rotation
+        input_data (ndarray): (mb_size, n_P, 3) data to be rotated
+    """
+    xp = chainer.cuda.get_array_module(input_data) # xp either numpy or cupy, based on GPU usage
+    thetas = xp.expand_dims(thetas, -1)
+
+    idx = [1,2]
+    xyz = xp.split(input_data, idx, axis=-1) # len 3 list, each of shape (mb_size, n_P, 1)
+    sin_t = xp.split(xp.sin(thetas), idx, axis=1) # len 3 list, each of shape (mb_size, 1, 1)
+    cos_t = xp.split(xp.cos(thetas), idx, axis=1)
+
+    rotated_z   = rotate_z(sin_t[0], cos_t[0], *xyz)
+    rotated_yz  = rotate_y(sin_t[1], cos_t[1], *rotated_z)
+    rotated_xyz = rotate_x(sin_t[2], cos_t[2], *rotated_yz)
+    out_data = xp.concatenate(rotated_xyz, axis=-1)
+    return out_data
+
+def random_augmentation_rotate(batches):
+    """ Randomly rotate data by theta
+    This function is redundant. No need to call rotate_3D twice if we
+    spend a little more time on indexing, but for now it stays
+    """
+    xp = chainer.cuda.get_array_module(batches[0]) # xp either numpy or cupy, based on GPU usage
+    mb_size = batches[0].shape[0]
+    thetas = xp.random.rand(mb_size, 3) * np.pi
+    out = []
+    for tmp in batches:
+        tmp_coo = rotate_3D(thetas, tmp[...,:3])
+        tmp_vel = rotate_3D(thetas, tmp[...,3:])
+        tmp = xp.concatenate((tmp_coo, tmp_vel), axis=-1).astype(xp.float32) # always remember float32 casting
+        tmp = normalize(tmp, scale_range=(-.125, .125)) # normalize casts
+        out.append(tmp)
+    return out
+
+def _deprecated_random_augmentation_shift(batches):
+    xp = chainer.cuda.get_array_module(batches[0])
+    batch_size = batches[0].shape[0]
+    out = []
+    rands = xp.random.rand(6)
+    shift = xp.random.rand(batch_size,3)
+    for tmp in batches:
+        if rands[0] < .5:
+            tmp = tmp[:,:,[1,0,2,4,3,5]]
+        if rands[1] < .5:
+            tmp = tmp[:,:, [0,2,1,3,5,4]]
+        if rands[2] < .5:
+            tmp = tmp[:,:, [2,1,0,5,4,3]]
+        if rands[3] < .5:
+            tmp[:,:,0] = 1 - tmp[:,:,0]
+            tmp[:,:,3] = -tmp[:,:,3]
+        if rands[4] < .5:
+            tmp[:,:,1] = 1 - tmp[:,:,1]
+            tmp[:,:,4] = -tmp[:,:,4]
+        if rands[5] < .5:
+            tmp[:,:,2] = 1 - tmp[:,:,2]
+            tmp[:,:,5] = -tmp[:,:,5]
+
+        tmploc = tmp[:,:,:3]
+        tmploc += shift[:,None,:]
+        gt1 = tmploc > 1
+        tmploc[gt1] = tmploc[gt1] - 1
+        tmp[:,:,:3] = tmploc
+        out.append(tmp)
+    return out
 
 def random_augmentation_shift(batch):
     """ Randomly augment data by shifting indices
@@ -217,9 +336,10 @@ def random_augmentation_shift(batch):
     Returns:
         batch (ndarray): randomly shifted data array
     """
+    xp = chainer.cuda.get_array_module(batch)
     batch_size = batch.shape[1]
-    rands = np.random.rand(6)
-    shift = np.random.rand(1,batch_size,1,3)
+    rands = xp.random.rand(6)
+    shift = xp.random.rand(1,batch_size,1,3)
     # shape (11, bs, n_P, 6)
     if rands[0] < .5:
         batch = batch[...,[1,0,2,4,3,5]]
@@ -260,10 +380,24 @@ def next_minibatch(X_in, batch_size, data_aug=True):
     else:
         return batches
 
+
+def _deprecated_next_minibatch(in_list, batch_size, data_aug='shift'):
+    num_samples, N, D = in_list[0].shape
+    index_list = np.random.choice(num_samples, batch_size)
+    batches = [in_list[k][index_list] for k in range(len(in_list))]
+    if data_aug == 'shift':
+        return random_augmentation_shift(batches)
+    elif data_aug == 'rotate':
+        #print('rotate aug')
+        return random_augmentation_rotate(batches)
+    else:
+        return batches
+
+
+
 def load_velocity_coefficients(num_particles):
     vel_coeffs = np.load('./Data/velocity_coefficients_{}.npy'.format(num_particles)).item()
     return vel_coeffs
-
 
 #=============================================================================
 # Saving utils
