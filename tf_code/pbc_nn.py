4324ecb2fe5a55ae7e1d337df6407e5e54731cea
Evan <evdcush@gmail.com>
2018-02-17 15:22:13 -0800

    tentatively finished implementation new pbc kneighbor, begin testing

--- tf_code/pbc_nn.py
+++ tf_code/pbc_nn.py
@@ -63,198 +63,116 @@ than it may be bounded to another part of the cube in the next redshift,
 like (0.4437, 0.0103, 0.2231)
 """
 #=============================================================================
-# maps and clones init
+# boundary utils
 #=============================================================================
-boundary_threshold = 0.1 # this represents absolute distance from a boundary
-N, D = x_in.shape # number of particles
+def face_outer(particle, bound): # ret shape (1,3)
+    # face only has one coordinate in boundary, so only one relocation
+    ret = bound + particle
+    return ret[None,:]
 
+def edge_outer(particle, bound):
+    # edge has two coordinates in boundary, so 3 relocations (edge, face, face)
+    zero_idx = list(bound).index(0)
+    edge = np.roll(np.array([[0,1,1],[0,1,0],[0,0,1]]), zero_idx, 1)
+    return (edge * bound) + particle
 
+def corner_outer(particle, bound): # ret shape (7, 3)
+    # corner has 3 coordinates in boundary, so 7 relocations:
+    # (corner, edge, edge, edge, face, face, face)
+    corner = np.array([[1,1,1],[1,1,0],[1,0,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1]])
+    return (corner * bound) + particle
 
-face_cubes   = {}
-edge_cubes   = {}
-corner_cubes = {}
-boundary_remap = {}
-
-expanded_cube = np.zeros((1,D))
-
-
-def map_to_cubes():
-    return false
-
-
-#=============================================================================
-# map and clone boundary particles
-#=============================================================================
-'''
-The computation flow:
- - original cube is cloned, all original indices preserved
- - for each particle original cube, if particle is boundary:
-     - add particle to cloned cube for as many dimensions as it needs to be
-        + corner particle: readjusted and added 7 times
-          eg (Top-front-left)
-           - opposite corner cube: (Top-front-left) to (Bottom-rear-right)
-           - opposite edge1 cube:  (Front-left) to (Rear-right)
-           - opposite edge2 cube:  (Top-left) to (Bottom-right)
-           - opposite edge3 cube:  (Top-front) to (Bottom-rear)
-           - opposite face1 cube: (Top) to (Bottom)
-           - opposite face2 cube: (Left) to (Right)
-           - opposite face3 cube: (Front) to (Rear)
-        + edge particle: readjusted and added 3 times
-          eg (Rear-right)
-           - opposite edge cube: (Rear-right) to (Front-left)
-           - opposite face1 cube: (Rear) to (Front)
-           - opposite face2 cube: (Right) to (Left)
-        + face particle: readjusted and added 1 time
-          eg (Bottom)
-           - opposite face cube: (Bottom) to (Top)
-     - each time the particle coordinates are readjusted and added, the index
-       to which they were are added are mapped back to their original location
- - kneighbors graph on expanded clone cube
- - for each particle within clone_cube[:N], if neighbors outside N, remap
-'''
-
-
-def shift(p):
-    """ p is (3,) coordinate vector, x, y, z
-    corner: all 3 coordinates in threshold
-    edge: 2 coordinates in threshold
-    face: 1 coord in threshold
-    """
-    x, y, z = p
-
-
-
-
-def build_boundary_dict(boundary_threshold):
-    """ Note: all cube location names, like 'Top' or 'right' are arbitrary
-    """
-    # get boundary range
-    lower = boundary_threshold
-    upper = 1 - boundary_threshold
-
-    # X Y Z
-    left_bottom_rear = (lower, lower, upper)
-
-def shift(_p, boundary_threshold):
-    """ p is (3,) coordinate vector, x, y, z
-    corner: all 3 coordinates in threshold
-    edge: 2 coordinates in threshold
-    face: 1 coord in threshold
-Just do the entire array at once for x<lower x>upper
-
-    """
-    p = np.copy(_p)
-    lower = boundary_threshold
-    upper = 1 - boundary_threshold
-
-    # get boundaries
-    boundary_upper = p >= upper
-    boundary_lower = p <= lower
-    b_upper_count = np.sum(boundary_upper)
-    b_lower_count = np.sum(boundary_lower)
-    num_boundary_dims = b_upper_count + b_lower_count
-
-    # ret
-    shifted = []
-    wrap = {()}
-
-    #if num_boundary_dims == 0: # not boundary particle
-    if num_boundary_dims > 0:
-        lower_wrap = np.where(boundary_lower, 1, 0)
-        upper_wrap = np.where(boundary_upper, -1, 0)
-        wrap = lower_wrap + upper_wrap
-        if num_boundary_dims == 1:
-            face = p + wrap
-            shifted.append(face)
-        elif num_boundary_dims == 2:
-            for i in range(b_lower_count):
-                face =
-
-            face1 = p + lower_wrap
-            face2 = p + upper_wrap
-            edge  = p + wrap
-
-
-
-
-        # corner: all coords change
-        # (.05, .05, .95) -> (1.05, 1.05, -0.05): (+1, +1, -1)
-        wrapped_upper = np.copy(p) + np.where(boundary_upper, -1, 0)
-        wrapped_lower = np.copy(p) + np.where(boundary_lower, 1, 0)
-        wrapped_corner = np.where(boundary_upper, wrapped_upper, wrapped_lower)
-
-        # edge: 2 coords change per
-        # (.05, .05, .95) -> (1.05, 1.05, .95): (+1, +1, 0)
-        wrapped_edge1 = np.copy(p) +
-        if b_upper_count > b_lower_count:
-            wrapped_edge = np.where(boundary_upper, wrapped_upper, p)
-
-        # faces: 1 coord change per
-
-
-
-    return shifted
-
-
-def pad_cube_boundaries(x, boundary_threshold=0.1):
-    """
-    """
-    # get boundary range
-    lower = boundary_threshold
-    upper = 1 - boundary_threshold
-
-    # clone original array
-    N, D = x.shape
-    clone = np.copy(x[...,:3])
-    idx_map = np.arange(N)
-
-    # iterate through array and add boundary particles to clone
-    for i in range(N):
-        p = clone[i:i+1] # (1, 3)
-
-
-
-
-def add_particle_to_cube(particle, cube):
-    """ Adds a boundary particle to expanded cube
-    All particles added to expanded cube. Boundary particles are readded and
-    their index in the expanded cube is added to map
-    Args:
-        particle (ndarray): nbody particle, a (1,6) vector
-        cube: the expanded cube to add the particle, of shape (*, 6),
-              initially None
-    Returns: cube with particle concatenated
-    """
-    if cube is None:
-        cube = particle
+def get_outer(particle, bound, num_boundary):
+    assert num_boundary > 0
+    if num_boundary == 1:
+        return face_outer(particle, bound)
+    elif num_boundary == 2:
+        return edge_outer(particle, bound)
     else:
-        cube = np.concatenate((cube, particle), axis=0)
-    return cube
+        return corner_outer(particle, bound)
 
-def add_boundary_particle_idx_to_map(bounded_idx, original_idx, bmap):
-    """ maps bounded particle to it's original location
+
+def pad_cube_boundaries(x, boundary_threshold):
+    """ check all particles for boundary conditions and
+    relocate boundary particles
+    For map, just make a 1D vector V where
+    V[i-N] = ith boundary particles original idx in X
+    V[(i+1)-N] = (i+1)th boundary particles original idx in X
+
+    I wonder if you could just do one corner_outer over the extracted corners
+    in x, edge_outer on extracted edges, and so forth, while saving indices?
+    Args:
+        x (ndarray): data array, shape (n_P, 3)
+    Returns: expanded x, index_list
     """
-    bmap[bounded_idx] = original_idx
-    return bmap
+    N, D = x.shape
+    idx_list = np.array([], dtype=np.int32) # keep in mind idx need to be offset by N
+
+    # boundary
+    lower = boundary_threshold
+    upper = 1 - boundary_threshold
+    bound_x = np.where(x >= upper, -1, np.where(x <= lower, 1, 0))
+    bound_x_count = np.count_nonzero(bound_x, axis=-1)
+
+    # get bound and add to clone
+    for idx in range(N):
+        num_boundary = bound_x_count[idx]
+        if num_boundary > 0:
+            # get particles to add to clone
+            outer_particles = get_outer(x[idx], bound_x[idx], num_boundary)
+            # add indices
+            idx_list = np.append(idx_list, [idx] * outer_particles.shape[0])
+            idx_list.extend(idx_to_add)
+            # concat to clone
+            x = np.concatenate((x, outer_particles), axis=0)
+    return x, idx_list
+
+def get_kneighbors_pcube(x, idx_map, N, K):
+    """ get kneighbor graph from padded cube
+    x is padded cube of shape (M, 3),
+    where M == (N + number of added boundary particles)
+    Args:
+        x (ndarray): padded cube, of shape (M, 3)
+        idx_map (ndarray): shape (M-N,) indices
+        N: number of particles in original cube
+        K: number of nearest neighbors
+    """
+    kgraph = kneighbors_graph(x, K, include_self=True)[:N].indices
+    kgraph_outer = kgraph >= N
+    for k_idx, is_outer in enumerate(kgraph_outer):
+        if is_outer:
+            kgraph[k_idx] = idx_map[k_idx - N]
+    return kgraph.reshape(N,K)
 
 
+def pbc_kneighbors(X, K, boundary_threshold=0.1):
+    """
+    """
+    # get boundary range
+    lower = boundary_threshold
+    upper = 1 - boundary_threshold
+    mb_size, N, D = X.shape
 
+    # graph init
+    adjacency_list = np.zeros((mb_size, N, K), dtype=np.int32)
 
+    for b in range(mb_size):
+        # get expanded cube
+        clone = np.copy(X[b,:,:3])
+        padded_cube, idx_map = pad_cube_boundaries(clone, boundary_threshold)
 
+        # get neighbors from padded_cube
+        kgraph_idx = get_kneighbors_pcube(padded_cube, idx_map)
+        adjacency_list[b] = kgraph_idx
+    return adjacency_list
 
-
-# JUST WORK ON SINGLE CUBE FOR NOW:
-# put things in map first
-for i in range(N):
-    x = x_in[i]
-    x_coo = x[:3]
 
 
 #=============================================================================
 # nearest neighbors graph and remap
 #=============================================================================
-'''
-def get_adjacency_list(X_in):
+
+def get_adjacency_list(X_in, K):
     """ search for K nneighbors, and return offsetted indices in adjacency list
     Args:
         X_in (numpy ndarray): input data of shape (mb_size, N, 6)
@@ -264,9 +182,9 @@ def get_adjacency_list(X_in):
     for i in range(mb_size):
         # this returns indices of the nn
         graph_idx = kneighbors_graph(X_in[i, :, :3], K, include_self=True).indices
-        graph_idx = graph_idx.reshape([N, K]) + (N * i)  # offset idx for batches
+        graph_idx = graph_idx.reshape([N, K]) #+ (N * i)  # offset idx for batches
         adj_list[i] = graph_idx
     return adj_list
-'''
 
-graph = kneighbors_graph()
+
+#=============================================================================
