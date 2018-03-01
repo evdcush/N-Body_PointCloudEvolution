0065316158486f4befed32efad5f1fa8c3c350e4
Evan <evdcush@gmail.com>
2018-03-01 13:01:52 -0800

    implemented scheduled sampling

--- tf_code/tf_multi_train.py
+++ tf_code/tf_multi_train.py
@@ -100,27 +100,24 @@ var_scopes = [utils.VAR_SCOPE_MULTI.format(j) for j in range(num_rs_layers)]
 
 
 # INPUTS
-#var_tags = ['X{}'.format(t) for t in utils.RS_TAGS.values()]
 data_shape = (num_rs, None, num_particles**3, 6)
 X_input = tf.placeholder(tf.float32, shape=data_shape, name='X_input')
 
 # ADJACENCY LIST
 alist_shape = (num_rs_layers, None, 2) # output shape
 adj_list = tf.placeholder(tf.int32, shape=alist_shape, name='adj_list')
-#adj_list = [tf.placeholder(tf.int32, shape=alist_shape, name='alist') for i in range(num_rs_layers)]
 
 # loss scaling weights
 scale_weights = tf.placeholder(tf.float32, shape=(num_rs_layers,), name='scale_weights')
 
+# scheduled sampling probs
+sampling_probs = tf.placeholder(tf.bool, shape=(num_rs_layers-1,), name='sampling_probs')
+
 def alist_func(h_in): # for tf.py_func
     return nn.alist_to_indexlist(nn.get_pbc_kneighbors(h_in, K, threshold))
 
-#alist_fn = tf.py_func(alist_func, [adj_list], tf.int32)
-#X_pred, loss = nn.multi_model_vel_fwd(X_input, var_scopes, num_layers, adj_list, K, scale_weights)
-X_pred, loss = nn.multi_model_fwd(X_input, var_scopes, num_layers, adj_list, K, scale_weights)
+X_pred, loss = nn.multi_model_fwd_sampling(X_input, var_scopes, num_layers, adj_list, K, scale_weights, sampling_probs)
 X_pred_val, loss_val = nn.multi_func_model_fwd(X_input, var_scopes, num_layers, alist_func, K)
-#X_pred_val, loss_val = nn.multi_func_model_vel_fwd(X_input, var_scopes, num_layers, alist_func, K)
-#X_pred, loss = nn.multi_func_model_fwd(X_input, var_scopes, num_layers, alist_func, K)
 
 # loss and optimizer
 train = tf.train.AdamOptimizer(learning_rate).minimize(loss)
@@ -159,8 +156,9 @@ for step in range(num_iters):
     x_in = _x_batch
     #code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
     sweights = nn.error_scales(np.copy(x_in))
+    sprobs = np.random.sample(num_rs_layers-1) < (step / num_iters)
     alist = [nn.alist_to_indexlist(nn.get_pbc_kneighbors(x_in[j], K, threshold)) for j in range(num_rs_layers)]
-    fdict = {X_input: x_in, adj_list: alist, scale_weights:sweights}
+    fdict = {X_input: x_in, adj_list: alist, scale_weights:sweights, sampling_probs:sprobs}
 
     if verbose:
         error = sess.run(loss, feed_dict=fdict)
