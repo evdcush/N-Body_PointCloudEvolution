d7ffa9f23d8ad379502ef0b29fc189f15a60ec31
Evan <evdcush@gmail.com>
2018-03-16 17:51:51 -0700

    cleaned up multi-train, redid sampling fwd

--- z_multi_train.py
+++ z_multi_train.py
@@ -32,7 +32,7 @@ start_time = time.time()
 num_particles = 32 #pargs['particles']
 redshift_steps = pargs['redshifts']
 #              0    1    2    3    4    5    6    7    8    9   10
-#REDSHIFTS = [6.0, 4.0, 2.0, 1.5, 1.2, 1.0, 0.8, 0.6, 0.4, 0.2, 0.0]
+#REDSHIFTS = [6.0, 4.0, 2.0, 1.5, 1.2, 1.0, 0.8, 0.6, 0.4, 0.2, 0.0] # old
 #redshift_steps = [0, 3, 5, 7, 10]
 num_rs = len(redshift_steps)
 num_rs_layers = num_rs - 1
@@ -93,7 +93,6 @@ vscope = utils.VAR_SCOPE_SINGLE_MULTI.format(zX, zY)
 tf.set_random_seed(utils.PARAMS_SEED)
 utils.init_params(channels, graph_model=use_graph, var_scope=vscope, restore=restore)
 
-
 # INPUTS
 #data_shape = (None, num_particles**3, 6)
 data_shape = (None, num_particles**3, 7)
@@ -104,26 +103,20 @@ X_truth = tf.placeholder(tf.float32, shape=data_shape, name='X_truth')
 alist_shape = (None, 2)
 adj_list = tf.placeholder(tf.int32, shape=alist_shape, name='adj_list')
 
-# loss scaling weights
-#scale_weights = tf.placeholder(tf.float32, shape=(num_rs_layers,), name='scale_weights')
-
-# scheduled sampling probs
-#sampling_probs = tf.placeholder(tf.bool, shape=(num_rs_layers-1,), name='sampling_probs')
-
 def alist_func(h_in): # for tf.py_func
-    #return nn.alist_to_indexlist(nn.get_pbc_kneighbors(h_in, K, threshold))
-    return nn.alist_to_indexlist(nn.get_kneighbor_alist(h_in, K))
+    return nn.alist_to_indexlist(nn.get_pbc_kneighbors(h_in, K, threshold))
+    #return nn.alist_to_indexlist(nn.get_kneighbor_alist(h_in, K))
 
 
 
-rs_adj_shape = (None, None, 2) # output shape
-#alist_shape = (None, 2)
-rs_adj = tf.placeholder(tf.int32, shape=rs_adj_shape, name='rs_adj')
 
+# SCHEDULED SAMPLING
+rs_adj = tf.placeholder(tf.int32, shape=(None, None, 2), name='rs_adj')
 full_rs_shape = (None, None, num_particles**3, 7)
 X_rs = tf.placeholder(tf.float32, shape=full_rs_shape, name='X_rs')
-sampling_probs = tf.placeholder(tf.bool, shape=(num_rs,), name='sampling_probs') # only needs to be num_rs -1, but for convenience
+sampling_probs = tf.placeholder(tf.bool, shape=(None,), name='sampling_probs')
 
+'''
 def multi_model_fwd_sampling(x_in, sampling_probs, adj):
     """
     Args:
@@ -136,9 +129,19 @@ def multi_model_fwd_sampling(x_in, sampling_probs, adj):
         h_in = tf.where(sampling_probs[i], tf.concat((h, x_in[i,:,:,-1]), axis=-1), x_in[i])
         h = nn.get_readout_vel(nn.zuni_model_fwd(h_in, num_layers, adj[i], K, var_scope=vscope))
     return h
-
-
-X_pred_train = multi_model_fwd_sampling(X_rs, sampling_probs, rs_adj)
+'''
+def multi_model_fwd_sampling(x_in, adj):
+    """
+    Args:
+        x_in: (11, mb_size, ...) full rs data
+    """
+    rs_depth = x_in.get_shape().as_list()[0]
+    h = nn.get_readout_vel(nn.zuni_model_fwd(x_in[0], num_layers, adj[0], K, var_scope=vscope))
+    if rs_depth is None: return h
+    for i in range(1, rs_depth):
+        #h_in = tf.where(sampling_probs[i], tf.concat((h, x_in[i,:,:,-1]), axis=-1), x_in[i])
+        h = nn.get_readout_vel(nn.zuni_model_fwd(h, num_layers, adj[i], K, var_scope=vscope))
+    return h
 
 
 #=============================================================================
@@ -153,18 +156,22 @@ else:
 # network out
 H_out  = nn.zuni_model_fwd(*margs, vel_coeff=vcoeff, var_scope=vscope)
 X_pred = nn.get_readout_vel(H_out)
+#X_pred_sched = multi_model_fwd_sampling(X_rs, sampling_probs, rs_adj)
+X_pred_sched = multi_model_fwd_sampling(X_rs, rs_adj)
 
-# error and optimizer
-single_error = nn.pbc_loss_vel(X_pred, X_truth[...,:-1])
-error = nn.pbc_loss_vel(X_pred_train, X_rs[-1, :,:, :-1])
+# training error
+single_step_error = nn.pbc_loss(X_pred, X_truth[...,:-1])
+scheduled_error   = nn.pbc_loss(X_pred_sched, X_rs[-1, :,:, :-1])
+
+# optimizer
 opt = tf.train.AdamOptimizer(learning_rate)
-train1 = opt.minimize(single_error)
-train2 = opt.minimize(error)
-ground_truth_error = nn.pbc_loss(X_input[...,:-1], X_truth[...,:-1])
+train1 = opt.minimize(single_step_error)
+train2 = opt.minimize(scheduled_error)
 
-# evaluation error (multi-step)
-Val_pred = tf.placeholder(tf.float32, shape=(None, num_particles**3, 6), name='Val_pred')
-multi_error = nn.pbc_loss(Val_pred, X_truth[...,:-1])
+# evaluation error
+X_pred_val = tf.placeholder(tf.float32, shape=(None, num_particles**3, 6), name='X_pred_val')
+val_error          = nn.pbc_loss(X_pred_val, X_truth[...,:-1])
+ground_truth_error = nn.pbc_loss(X_input[...,:-1], X_truth[...,:-1])
 
 
 #=============================================================================
@@ -176,14 +183,14 @@ num_iters  = pargs['num_iters']
 verbose    = pargs['verbose']
 
 # Sess
-gpu_frac = 0.8
+gpu_frac = 0.85
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_frac)
 sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
 #sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 if restore:
     utils.load_graph(sess, model_path)
-#code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
+
 # Save
 train_loss_history = np.zeros((num_iters)).astype(np.float32)
 saver = tf.train.Saver()
@@ -225,25 +232,26 @@ for step in range(num_iters):
         saver.save(sess, model_path + model_name, global_step=step, write_meta_graph=True)
 print('FINISHED SINGLE-STEP TRAINING')
 
+
 for step in range(num_iters):
-    # data batching
     #print('STEP: {}'.format(step))
+
+    # data batching: (num_rs, mb_size, N, 7)
     _x_batch = utils.next_zuni_minibatch(X_train, batch_size, data_aug=True)
-    '''
-    np.random.shuffle(rs_tups)
-    for idx_in, idx_out in rs_tups:
-        # data inputs
-        x_in    = _x_batch[idx_in]
-        x_truth = _x_batch[idx_out]
-        fdict = {X_input: x_in, X_truth: x_truth}
 
-        # feed graph model data
-        if use_graph:
-            alist = alist_func(x_in)
-            fdict[adj_list] = alist
+    # sampling probabilities
+    rands = np.random.rand(num_rs) <= step / (float(num_iters) * 0.85)
+    pred_depth = np.sum(rands)
+    if pred_depth < 1: pred_depth += 1
 
-        # training pass
-        train.run(feed_dict=fdict)
+    # feed data
+    d = -(pred_depth + 1)
+    x_in = _x_batch[d:]
+    adj_lists = np.array([alist_func(x_in[i]) for i in range(x_in.shape[0])])
+    fdict = {X_rs: x_in, rs_adj:adj_lists}
+    train2.run(feed_dict=fdict)
+
+
     '''
     # sampling probs: whether the input at rs[i] is true or pred
     samp_probs = np.random.rand(num_rs) <= step / float(num_iters)
@@ -254,18 +262,19 @@ for step in range(num_iters):
     depth_probs = np.random.rand(num_rs) <= w
     depth_probs[0] = True
     pred_depth = np.sum(depth_probs)
-    #print('{}: samp: {}, depth: {}'.format(step, samp_probs, pred_depth))
 
     x_in = _x_batch[:pred_depth+1]
     adj_lists = np.array([alist_func(x_in[i]) for i in range(pred_depth)])
     fdict = {X_rs: x_in, rs_adj:adj_lists, sampling_probs:samp_probs}
     train2.run(feed_dict=fdict)
+    '''
 
     # save checkpoint
     if save_checkpoint(step):
         #tr_error = sess.run(error, feed_dict=fdict)
         #print('checkpoint {:>5}: {}'.format(step, tr_error))
         saver.save(sess, model_path + model_name, global_step=step, write_meta_graph=True)
+
 # END
 print('elapsed time: {}'.format(time.time() - start_time))
 
@@ -313,11 +322,11 @@ for j in range(X_test.shape[1]):
     # feeding for multi-step loss info
     x_in   = X_test[-2, j:j+1]
     x_true = X_test[-1, j:j+1]
-    fdict = {Val_pred: x_pred, X_input: x_in, X_truth: x_true}
+    fdict = {X_pred_val: x_pred, X_input: x_in, X_truth: x_true}
 
     # loss data
     #code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
-    v_error, truth_error = sess.run([multi_error, ground_truth_error], feed_dict=fdict)
+    v_error, truth_error = sess.run([val_error, ground_truth_error], feed_dict=fdict)
     test_loss_history[j] = v_error
     inputs_loss_history[j] = truth_error
     '''
