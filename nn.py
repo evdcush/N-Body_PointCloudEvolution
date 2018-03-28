79cd961f45fd72cb57198697621cdc89470a807f
Evan <evdcush@gmail.com>
2018-03-27 19:20:14 -0700

    simplified indexing for agg fns

--- nn.py
+++ nn.py
@@ -199,9 +199,10 @@ def zuni_multi_single_fwd(x_rs, num_layers, rs_adj_list, K, var_scopes):
     concat_rs = lambda x, z: tf.concat((x, z), axis=-1)
     fwd = lambda x, a, v: get_readout_vel(zuni_model_fwd(x, num_layers, a, K, var_scope=v))
     h = fwd(x_rs[0], rs_adj_list[0], var_scopes[0])
-    for i, vscope in enumerate(var_scopes[1:]):
-        h_in = concat_rs(h, x_rs[i+1,:,:,-1:])
-        h = fwd(h_in, rs_adj_list[i+1], vscope)
+    for i in range(1, len(var_scopes)):
+        vscope = var_scopes[i]
+        h_in = concat_rs(h, x_rs[i,:,:,-1:])
+        h = fwd(h_in, rs_adj_list[i], vscope)
     return h
 
 
@@ -215,10 +216,11 @@ def zuni_multi_single_fwd_vel_losses(x_rs, num_layers, rs_adj_list, K, var_scope
     h = fwd(x_rs[0], rs_adj_list[0], var_scopes[0])
     vel_error = verror(h, x_rs[1])
 
-    for i, vscope in enumerate(var_scopes[1:]):
-        h_in = concat_rs(h, x_rs[i+1,:,:,-1:])
-        h = fwd(h_in, rs_adj_list[i+1], vscope)
-        vel_error += verror(h, x_rs[i+1])
+    for i in range(1, len(var_scopes)):
+        vscope = var_scopes[i]
+        h_in = concat_rs(h, x_rs[i,:,:,-1:])
+        h = fwd(h_in, rs_adj_list[i], vscope)
+        vel_error += verror(h, x_rs[i])
     return h, vel_error
 
 def zuni_multi_single_fwd_val(x_rs, num_layers, adj_fn, K, var_scopes):
@@ -228,8 +230,9 @@ def zuni_multi_single_fwd_val(x_rs, num_layers, adj_fn, K, var_scopes):
     adj = tf.py_func(adj_fn, [x_rs[0]], tf.int32)
 
     h = fwd(x_rs[0], adj, var_scopes[0])
-    for i, vscope in enumerate(var_scopes[1:]):
-        h_in = concat_rs(h, x_rs[i+1,:,:,-1:])
+    for i in range(1, len(var_scopes)):
+        vscope = var_scopes[i]
+        h_in = concat_rs(h, x_rs[i,:,:,-1:])
         adj = tf.py_func(adj_fn, [h_in], tf.int32)
         h = fwd(h_in, adj, vscope)
     return h
@@ -243,8 +246,9 @@ def zuni_multi_single_fwd_val_all(x_rs, num_layers, adj_fn, K, var_scopes):
 
     h = fwd(x_rs[0], adj, var_scopes[0])
     preds.append(h)
-    for i, vscope in enumerate(var_scopes[1:]):
-        h_in = concat_rs(h, x_rs[i+1,:,:,-1:])
+    for i in range(1, len(var_scopes)):
+        vscope = var_scopes[i]
+        h_in = concat_rs(h, x_rs[i,:,:,-1:])
         adj = tf.py_func(adj_fn, [h_in], tf.int32)
         h = fwd(h_in, adj, vscope)
         preds.append(h)
