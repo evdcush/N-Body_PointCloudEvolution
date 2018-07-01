8bd279313387526805104379e29ff4259fdc91e5
evan <evdcush@gmail.com>
2018-06-30 21:04:08 -0700

    major cleanups in vis_multi, maybe working input norm;

--- Visualization/visualize_multi.py
+++ Visualization/visualize_multi.py
@@ -1,6 +1,6 @@
 ''' notebook for visualizing results in matplotlib
 '''
-import os, code, sys, time, argparse
+import os, code, sys, time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,15 +8,18 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.neighbors import kneighbors_graph
 import tensorflow as tf
 
-import utils
-from utils import REDSHIFTS, PARAMS_SEED, LEARNING_RATE, RS_TAGS
-
 #=============================================================================
 # data vars
 #=============================================================================
+REDSHIFTS_ZUNI = [9.0000, 4.7897, 3.2985, 2.4950, 1.9792, 1.6141, 1.3385,
+                 1.1212, 0.9438, 0.7955, 0.6688, 0.5588, 0.4620, 0.3758,
+                 0.2983, 0.2280, 0.1639, 0.1049, 0.0505, 0.0000]
+
 # paths
-dpath = '../multi_9k/{}X32_11-19_{}.npy'
+dpath = './X32_11-19_{}.npy'
 spath = './Imgs/'
+if not os.path.exists(spath): os.makedirs(spath)
+
 
 # load fn
 def load_cube(mname, true_data=False):
@@ -26,17 +29,18 @@ def load_cube(mname, true_data=False):
         return np.load(dpath.format(mname, 'prediction'))
 
 # model names
-pred_mname = 'SC_NOMU_'
+mname = 'ScaledLoss_ShiftInvariant'
 
 # redshift vars
 redshift_steps = [11, 15, 19] # reverse sorted indices into redshifts. redshifts[19] == redshifts[-1] == 0.0000
-redshifts = [utils.REDSHIFTS_ZUNI[i] for i in redshift_steps] # actual redshift values
+redshifts = [REDSHIFTS_ZUNI[i] for i in redshift_steps] # actual redshift values
 num_rs = len(redshift_steps)
 num_rs_layers = num_rs - 1
 
 # load
-X_true = load_cube('', True)
-X_pred = load_cube(pred_mname)
+X_true = np.load(dpath.format('true'))
+X_pred = np.load(dpath.format('prediction'))
+test_error = np.load(dpath.format('loss_validation'))
 
 
 #=============================================================================
@@ -44,14 +48,9 @@ X_pred = load_cube(pred_mname)
 #=============================================================================
 AX_LABEL_SIZE = 20
 BOUND = 0.1
-split_coo = lambda x: np.split(x[...,:3],3,axis=-1)
-split_vel = lambda x: np.split(x[...,3:],3,axis=-1)
 
 def split_xyz(x, vel=False):
-    if vel:
-        return split_vel(x)
-    else:
-        return split_coo(x)
+    return np.split(x, 3, axis=-1)
 
 def get_samples(x_true, x_hat, rs_idx, sample_idx):
     j = sample_idx
@@ -62,8 +61,8 @@ def get_samples(x_true, x_hat, rs_idx, sample_idx):
 
 def plot_3D_pointcloud_ax(ax, x_true, x_hat, rs_idx, sample_idx, pt_size=(.9,.9), colors=('r','b'), fsize=(18,18)):
     xt, xh = get_samples(x_true, x_hat, rs_idx, sample_idx)
-    xt_xyz = split_xyz(xt)
-    xh_xyz = split_xyz(xh)
+    xt_xyz = split_xyz(xt[...,:3])
+    xh_xyz = split_xyz(xh[...,:3])
     # fig
     #fig = plt.figure(figsize=fsize)
     #ax = fig.add_subplot(111, projection='3d')
@@ -77,15 +76,7 @@ def plot_3D_pointcloud_ax(ax, x_true, x_hat, rs_idx, sample_idx, pt_size=(.9,.9)
     #return fig
 
 def plot_quiver_axes(ax, coo, vel, **quiver_kwargs):
-    #ax.quiver(*coo, *vel, **quiver_kwargs)
-    xc = coo[:,:1]
-    yc = coo[:,1:2]
-    zc = coo[:,2:3]
-    xv = vel[:,:1]
-    yv = vel[:,1:2]
-    zv = vel[:,2:3]
-
-    ax.quiver(xc, yc, zc, xv, yv, zv, **quiver_kwargs)
+    ax.quiver(*coo, *vel, **quiver_kwargs)
 
 def get_mask(x, bound=BOUND):
     xtmp = x[...,:3]
@@ -97,64 +88,55 @@ def get_mask(x, bound=BOUND):
     mask_nz = np.nonzero(mask)[0]
     return mask_nz
 
-def mask_data(x, mask):
-    return x[mask]
-
-def plot_3D_quiver_ax(ax, x_true, x_hat, rs_idx, sample_idx, pt_size=(.9,.9), colors=('r','b'), fsize=(18,18)):
+def plot_3D_quiver_ax(ax, x_true, x_hat, rs_idx, sample_idx):
     xt, xh = get_samples(x_true, x_hat, rs_idx, sample_idx)
     xin = x_true[rs_idx-1, sample_idx]
     mask = get_mask(xin)
 
     # mask
-    xt = mask_data(xt, mask)
-    xh = mask_data(xh, mask)
-    xin = mask_data(xin, mask)
+    xt  = xt[mask]
+    xh  = xh[mask]
+    xin = xin[mask]
 
     # data vecs
-    #xt_coo  = split_xyz(xt)
-    #xh_coo  = split_xyz(xh)
-    #xin_coo = split_xyz(xin)
     xt_coo  = xt[...,:3]
     xh_coo  = xh[...,:3]
     xin_coo = xin[...,:3]
     xin_vel = xin[...,3:]
 
-    # fig
-    #fig = plt.figure(figsize=fsize)
-    #ax = fig.add_subplot(111, projection='3d')
-
     # arrows
-    arrow_input = (xin_coo, xin_vel)
-    arrow_truth = (xt_coo, xt_coo - xin_coo)
-    arrow_pred  = (xh_coo, xh_coo - xin_coo)
+    arrow_input = split_xyz(xin_coo) + split_xyz(xin_vel)
+    arrow_truth = split_xyz(xt_coo ) + split_xyz(xt_coo - xin_coo)
+    arrow_pred  = split_xyz(xh_coo ) + split_xyz(xh_coo - xin_coo)
 
     # plot
-    quiver_kwargs = {'pivot': 'middle', 'length': 1.0, 'normalize': False, 'alpha': 0.4, 'linewidths': 0.5, 'scale':2.3}
-    #arrow_true = (split_xyz(xin), spl)
-    #coo = split_xyz(x)
-    #vel = split_xyz(x, vel=True)
-    #code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
-    plot_quiver_axes(ax, *arrow_truth, **quiver_kwargs, color='r',)
-    #plot_quiver_axes(ax, *arrow_pred,  **quiver_kwargs, color='b', scale=2.3)
-    #plot_quiver_axes(ax, *arrow_input, **quiver_kwargs, color='g', scale=np.max(xin_vel))
+    quiver_kwargs = {'pivot': 'middle', 'linewidths': 0.5, }#'alpha': 0.4,}
 
-    # label
+    #code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
+    ax.quiver(*arrow_input, **quiver_kwargs, color='g', alpha=0.4, length=0.01)
+    ax.quiver(*arrow_truth, **quiver_kwargs, color='r', alpha=0.2) # red a bit too strong
+    ax.quiver(*arrow_pred,  **quiver_kwargs, color='b', alpha=0.4)
+
+    # Axis labels
     ax.set_xlabel('X', size=AX_LABEL_SIZE)
     ax.set_ylabel('Y', size=AX_LABEL_SIZE)
     ax.set_zlabel('Z', size=AX_LABEL_SIZE)
-    #return fig
+
+    # Legend labels
+    labels = ['Input', 'Truth', 'Pred']
+    leg = ax.legend(labels, loc=(0.8,0.82))
+    for line, text in zip(leg.get_lines(), leg.get_texts()):
+        line.set_linewidth(4)
+        text.set_fontsize('large')
+
 
 #=============================================================================
 # graph ops
 #=============================================================================
-
-k = 3
+# Figure/plot properties
+k = 10
 fsize = (k*num_rs_layers,k) # (w, h)
-#fsize = (k*4,k*2)
 particle_size = (.8,.8)
-truth_color = 'red'
-pred_color = 'blue'
-colors = (truth_color, pred_color)
 
 xt = np.copy(X_true)
 xh = np.copy(X_pred)
@@ -162,21 +144,19 @@ xh = np.copy(X_pred)
 sample_idx = 39 #185
 #rs_idx = 4
 #idx_tup = (rs_idx, sample_idx)
-
-plt.close('all')
-#fig = plot_3D_pointcloud(xt, xh, *idx_tup, colors=colors, pt_size=particle_size, fsize=fsize)
-fig = plt.figure(figsize=fsize)
 """
 Pyplot subplot numbers:
- - 111: 1x1 grid, first subplot
- - 234: 2x3 grid, 4th subplot
+    - 111: 1x1 grid, first subplot
+    - 234: 2x3 grid, 4th subplot
 """
-#subplots_arrow = [245,246,247,248]
-#subplots_arrow = [141,142,143]
+
+plt.close('all')
+fig = plt.figure(figsize=fsize)
 num_cols = num_rs_layers
-subplots_arrow = [int('1{}{}'.format(num_cols, i)) for i in range(1, num_rs)]
-#subplots_point = [241,242,243,244]
-rs_indices = [i for i in range(1, num_rs)]
+subplots_idx = [int('1{}{}'.format(num_cols, i)) for i in range(1, num_rs)]
+rs_idx = [i for i in range(1, num_rs)]
+
+
 '''
 for subplot, rs_idx in zip(subplots_point, rs_indices):
     print('POINT redshifts[{}] = {}'.format(rs_idx, redshifts[rs_idx]))
@@ -185,44 +165,28 @@ for subplot, rs_idx in zip(subplots_point, rs_indices):
     ax.title.set_text(ax_title)
     plot_3D_pointcloud_ax(ax, xt, xh, rs_idx, sample_idx, colors=colors, pt_size=particle_size, fsize=fsize)
 '''
-test_error = np.load('./multi_9k/{}ZG_11-19_loss_validation.npy'.format(pred_mname))
-for subplot, rs_idx in zip(subplots_arrow, rs_indices):
-    print('ARROW redshifts[{}] = {}'.format(rs_idx, redshifts[rs_idx]))
-    med_err = np.median(test_error[:,rs_idx-1])
-    ax_title = 'Redshift {:.4f}\nmedian error: {:.5f}'.format(redshifts[rs_idx], med_err)
-    ax = fig.add_subplot(subplot, projection='3d')
-    #ax.title.set_text(ax_title, (0.5, .8))
-    ax.set_title(ax_title, pad=0.3)
-    plot_3D_quiver_ax(ax, xt, xh, rs_idx, sample_idx, colors=colors, pt_size=particle_size, fsize=fsize)
 
-#test_medians = [7.699121488258243e-05, 5.8502777392277494e-05, 5.3116607887204736e-05, 4.708175401901826e-05]
-#mu = np.mean(test_medians)
+def plot_particles(cube_idx):
 
-mu = np.median(test_error[:,-1])
-fig.suptitle("Scaled Loss, Single-step only training, 9K iters", size=24)
+
+
+def plot_arrows(cube_idx):
+    for subplot, zi in zip(subplots_idx, rs_idx):
+        print('ARROW redshifts[{}] = {}'.format(zi, redshifts[zi]))
+        median_error = np.median(test_error[:,zi-1])
+        ax_title = 'Redshift {:.4f}\nmedian error: {:.5f}'.format(redshifts[zi], median_error)
+        ax = fig.add_subplot(subplot, projection='3d')
+        ax.set_title(ax_title, pad=0.3)
+        plot_3D_quiver_ax(ax, xt, xh, zi, cube_idx)
+    ftitle = '{} Displacement'.format(mname)
+    fig.suptitle(ftitle, size=24)
+
+plot_arrows(sample_idx)
 
 plt.tight_layout()
-#plt.subplots_adjust(left=0.125, right=0.9, bottom=0.1, top=0.9, wspace=0.2, hspace=0.2)
-#plt.gca().view_init(20, 120)
-#display.display(plt.gcf())
-#display.clear_output(wait=True)
-#time.sleep(0.0001)
-'''
-rotate = False
-if rotate:
-    for angle in range(0,360, 60):
-        #fig.view_init(30, angle)
-        plt.gca().view_init(30, angle)
-        display.display(plt.gcf())
-        display.clear_output(wait=True)
-        time.sleep(0.0001)
-'''
-#fig.savefig(spath + 'test', dpi=2400,bbox_inches='tight') # warning, this makes a huge image
-#fig.savefig(spath + 'IndividuallyTrained_arrows', dpi=1000, bbox_inches='tight') # warning, this makes a huge image
-
 #fig.savefig(spath + 'Scaled_no_mult2', dpi=1000, bbox_inches='tight') # warning, this makes a huge image
 #fig.savefig(spath+ 'test')
 print('finished')
 
 plt.show()
-code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
+#code.interact(local=dict(globals(), **locals())) # DEBUGGING-use
